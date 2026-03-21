1b104cdfe3badc925b5e3faa5d1f989c4641e49c
sokacheski <sokacheskicorp@gmail.com>
2026-03-21 19:59:47 -0300

    Corrige overlay de cursos bloqueados: centralizado e mensagem fixa

--- client/src/pages/members.tsx
+++ client/src/pages/members.tsx
@@ -535,7 +535,7 @@ export default function MembersPage() {
                               className="absolute inset-0 w-full h-full object-cover"
                             />
                             
-                            {/* OVERLAY DE BLOQUEIO */}
+                            {/* OVERLAY DE BLOQUEIO - SEMPRE VISÍVEL E CENTRALIZADO */}
                             {isPaidAndNotPurchased && (
                               <CourseOverlay 
                                 type="paid" 
